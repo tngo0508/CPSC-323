7b350a2a6fad77b83d07ca2aa1fc20bced7a62c5
Nguyen Tran <nguyentran@Nguyens-MacBook-Pro.local>
2017-09-18 11:53:13 -0700

    update new source code

---
+++ assignment_1/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <regex>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	string s[3] = { "abc##","abcd#", "a##abcd" };
+
+
+	//regex r("\\w(\\w+#)|(\\w#\\w*)*");
+	regex r("(\\w+#?)+");
+	//regex r("\\w((\\w*#)|\\w*(#\\w+)*)");
+
+	//smatch m;
+
+	////
+	//regex_search(s, m, r);
+
+	//for (auto x : m)
+	//	cout << x << " ";
+
+	/*if (regex_match(s, r)) {
+		cout << "TRUE" << endl;
+	}
+	*/
+
+	smatch sm;
+	for (int j = 0; j <3; ++j) {
+		regex_match(s[j], sm, r, regex_constants::match_default);
+		std::cout << "the matches were: ";
+		for (unsigned i = 0; i< 1 ;++i) {
+			std::cout << "[" << sm[i] << "] ";
+		}
+		cout << endl;
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
