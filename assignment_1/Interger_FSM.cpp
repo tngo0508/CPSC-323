bd28da27601b9c74c63c85400e61633e798ba7bd
thomas ngo <tngo0508@gmail.com>
2017-09-20 09:01:29 -0700

    update FSM for integer and main.cpp

--- assignment_1/Interger_FSM.cpp
+++ assignment_1/Interger_FSM.cpp
@@ -28,5 +28,22 @@ int DFSM(string arg)
 }
 
 int main() {
+    ofstream file;
+    file.open("test_case.txt");
+    
+    //Make sure file is opened
+    if (!file.is_open())
+    {
+        cerr << "Failed to open the file";
+        exit(1);
+    }
+    
+    while (!file.eof())
+    {
+        //call function lexer
+        //print(token, lexeme)
+    }
+
+    file.close();
 	return 0;
 }
---
+++ assignment_1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <ctype.h>
+
+using namespace std;
+
+//Finite State Machine for integer
+int int_DFSM(string arg)
+{
+    //create table N for the transitions
+    int a[3][2] = {0, 'd', 1, 12, 12, 12};
+
+    //accepting states
+    int f[0] = {12};
+
+    int col = 1;
+    int state = 1; //starting state
+    
+    int size = arg.size();
+    for (int i = 0; i < size; i++ )
+    {
+        state = a[state,col];
+    }
+    if (state == f[0])
+        return 1; //accepted
+    else 
+        return 0; //rejected
+}
+
+int main() {
+    ofstream file;
+    file.open("test_case.txt");
+    
+    //Make sure file is opened
+    if (!file.is_open())
+    {
+        cerr << "Failed to open the file";
+        exit(1);
+    }
+    
+    while (!file.eof())
+    {
+        //call function lexer
+        //print(token, lexeme)
+    }
+
+    file.close();
+	return 0;
+}
