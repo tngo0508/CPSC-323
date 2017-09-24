8f430ee99ee6aed7372cb93ff8594e4e462c9869
thomas ngo <tngo0508@gmail.com>
2017-09-23 20:53:29 -0700

    write implementation for FSM-real

---
+++ assignment_1/Lex.hpp
@@ -0,0 +1,26 @@
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctype.h>
+
+using namespace std;
+
+class Lex
+{
+    public:
+        Lex();
+        int int_DFSM(int& currentState, const char input);
+        int real_DFSM(int& currentState, const char input);
+        int char_to_col(const char input) const;
+        string lexer(ifstream& file);
+        ~Lex();
+    private:
+        int currentState;
+        char input;
+};
+
+#endif
