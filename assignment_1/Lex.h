b274af34b0398fc1a9c57d4b11da98cda14ef3fe
thomas ngo <tngo0508@gmail.com>
2017-09-25 23:49:55 -0700

    add some functions to header

---
+++ assignment_1/Lex.h
@@ -0,0 +1,36 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctype.h>
+
+using namespace std;
+
+class Lex
+{
+    public:
+        Lex();
+        int int_DFSM(const string str);
+        int real_DFSM(const string str);
+        int identifier_DFSM(const string str);
+        int char_to_col(const char input) const;
+        bool isSeparator(const char input) const;
+        bool isOperator(const char input) const; 
+        bool checkKeyword(string identifier) const;
+        void lexer(ifstream& file);
+        void print() const;
+        void setToken(const string newToken);
+        void setLexeme(const string newLexeme);
+        void getToken() const;
+        void getLexeme() const;
+        ~Lex();
+    private:
+        char input;
+        string lexeme;
+        string token;
+};
+
+#endif
