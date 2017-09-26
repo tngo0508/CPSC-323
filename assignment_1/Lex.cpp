94479d16b654f6a936393473dda462c16b05ceac
Thomas Ngo <tngo0508@gmail.com>
2017-09-26 10:02:25 -0700

    debug the program

--- assignment_1/Lex.cpp
+++ assignment_1/Lex.cpp
@@ -2,14 +2,14 @@
 
 Lex::Lex()
 {
-    input = 'c';
+	input = 'c';
 }
 
 //Function check separator
 bool Lex::isSeparator(const char input) const
 {
 	int separator[6] = { '(',')', '{', '}', '%', '@' };
-	for (int i = 0; i < 6; i++ ) {
+	for (int i = 0; i < 6; i++) {
 		if (separator[i] == input) {
 			return 1;
 		}
@@ -19,141 +19,141 @@ bool Lex::isSeparator(const char input) const
 //Function check Operator
 bool Lex::isOperator(const char input) const
 {
-    int operators[10] = { '+', '-', '/', '*', '<','>','=',':','!', '=' };
-    for (int i = 0; i < 10; i++) {
-        if (operators[i] == input) {
-            return 1;
-        }
-    }
-    return 0;
+	int operators[12] = { '(', ')', '+', '-', '/', '*', '<','>','=',':','!', '=' };
+	for (int i = 0; i < 12; i++) {
+		if (operators[i] == input) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 //Function check keyword
 bool Lex::checkKeyword(string identifier) const
 {
-    string keywords[12] = { "while", "if", "int", "fi", "else", "return",
-        "read", "write", "integer", "for", "string", "boolean" };
-    for (int i = 0; i < 12; i++) {
-        if (keywords[i] == identifier) {
-            return 1;
-        }
-    }
-    return 0;
+	string keywords[12] = { "while", "if", "int", "fi", "else", "return",
+		"read", "write", "integer", "for", "string", "boolean" };
+	for (int i = 0; i < 12; i++) {
+		if (keywords[i] == identifier) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 //Function returns the column number of the character in the table
 int Lex::char_to_col(const char input) const
 {
-    if (isdigit(input))
-        return 1;
-    else if (input == '.')
-        return 2;
-    else if (isalpha(input))
-        return 3;
-    else
-        return 4;
+	if (isdigit(input))
+		return 1;
+	else if (input == '.')
+		return 2;
+	else if (isalpha(input))
+		return 3;
+	else
+		return 4;
 }
 
 //Finite State Machine for integer
 int Lex::int_DFSM(const string str)
 {
-    //starting state
-    int state = 1;
+	//starting state
+	int state = 1;
 
-    //create table N for the transitions
-    int a[3][2] = {0, 'd', 1, 2, 2, 2};
+	//create table N for the transitions
+	int a[3][2] = { 0, 'd', 1, 2, 2, 2 };
 
-    //accepting states
-    int f[1] = {2};
+	//accepting states
+	int f[1] = { 2 };
 
-    //update the currentState to new transition
-    int size = str.size(); 
-    for (int i = 0; i < size; i++)
-    {
-        int col = char_to_col(str[i]);
-        if (col > 1)
-            return 0;
-        state = a[state][col];
-    }
-    if (state == f[0])
-        return 1;
-    else
-        return 0;
+	//update the currentState to new transition
+	int size = str.size();
+	for (int i = 0; i < size; i++)
+	{
+		int col = char_to_col(str[i]);
+		if (col > 1)
+			return 0;
+		state = a[state][col];
+	}
+	if (state == f[0])
+		return 1;
+	else
+		return 0;
 }
 
 //Finite State Machine for real
 int Lex::real_DFSM(string str)
 {
-    int state = 1;
-    int a[5][3] = {0, 'd', '.', 1, 2, 0, 2, 2, 3, 3, 4, 0, 4, 4, 0};
+	int state = 1;
+	int a[5][3] = { 0, 'd', '.', 1, 2, 0, 2, 2, 3, 3, 4, 0, 4, 4, 0 };
 
-    int f[1] = {4};
+	int f[1] = { 4 };
 
-    //convert character to its column number in the table
-    int size = str.size();
-    for (int i = 0; i < size; i++)
-    {
-        int col = char_to_col(str[i]);
-        if (col > 2)
-            return 0;
-        state = a[state][col];
-    }
-    if (state == f[0])
-        return 1;
-    else
-        return 0;
+	//convert character to its column number in the table
+	int size = str.size();
+	for (int i = 0; i < size; i++)
+	{
+		int col = char_to_col(str[i]);
+		if (col > 2)
+			return 0;
+		state = a[state][col];
+	}
+	if (state == f[0])
+		return 1;
+	else
+		return 0;
 }
 
 //Finite State Machine for identifier
 int Lex::identifier_DFSM(string str)
 {
-    int state = 1;
-    int a[6][5] = {0, 'd', '.', 'l', '#', 1, 0, 0, 2, 0, 2, 0, 0, 3, 4, 3, 0, 0,
-        3, 4, 4, 0, 0, 5, 0, 5, 0, 0, 3, 4};
+	int state = 1;
+	int a[6][5] = { 0, 'd', '.', 'l', '#', 1, 0, 0, 2, 0, 2, 0, 0, 3, 4, 3, 0, 0,
+		3, 4, 4, 0, 0, 5, 0, 5, 0, 0, 3, 4 };
 
-    int f[4] = {2, 3, 4, 5};
+	int f[4] = { 2, 3, 4, 5 };
 
-    //convert character to its column number in the table
-    int size = str.size();
-    for (int i = 0; i < size; i++)
-    {
-        int col = char_to_col(str[i]);
-        state = a[state][col];
-    }
+	//convert character to its column number in the table
+	int size = str.size();
+	for (int i = 0; i < size; i++)
+	{
+		int col = char_to_col(str[i]);
+		state = a[state][col];
+	}
 
-    for (int i = 0; i < 4; i++)
-    {
-        if (state == f[i])
-            return 1;
-    }
-    return 0;
+	for (int i = 0; i < 4; i++)
+	{
+		if (state == f[i])
+			return 1;
+	}
+	return 0;
 }
 
 void Lex::lexer(ifstream& file)
 {
-    string str;
-    int state_status = 0; 
-    bool found = false;
-    char ch = 'c';
-    
-    while (!found)
-    {
-        ch = file.get();
-        if (this->isSeparator(ch) || this->isOperator(ch)  || ch == 32)
-        {
-            found = true;
-            file.unget();
-        }
-            str += ch;
-    }
+	string str;
+	int state_status = 0;
+	bool found = false;
+	char ch = 'c';
+
+	while (!found)
+	{
+		ch = file.get();
+		if (this->isSeparator(ch) || this->isOperator(ch) || ch == 32)
+		{
+			found = true;
+		}
+		else
+			str += ch;
+	}
 
 	if (str[0] == ' ')
 		ch = file.get();
 	else if (this->isOperator(ch))
 	{
 		this->setToken("operator");
+		str = ch;
 		this->setLexeme(str);
-		ch = file.get();
 	}
 	else if (this->isSeparator(ch))
 	{
@@ -183,18 +183,18 @@ void Lex::lexer(ifstream& file)
 
 void Lex::print() const
 {
-    // cout << "token" << "        " << "lexeme";
-    cout << this->token << " "<< this->lexeme << endl;
+	// cout << "token" << "        " << "lexeme";
+	cout << this->token << " " << this->lexeme << endl;
 }
 
 void Lex::setToken(const string newToken)
 {
-    token = newToken;
+	token = newToken;
 }
 
 void Lex::setLexeme(const string newLexeme)
 {
-    lexeme = newLexeme;
+	lexeme = newLexeme;
 }
 
 string Lex::getToken() const
@@ -207,4 +207,4 @@ string Lex::getLexeme() const
 	return lexeme;
 }
 
-Lex::~Lex(){}
+Lex::~Lex() {}
