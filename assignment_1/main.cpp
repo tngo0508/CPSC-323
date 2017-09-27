62a09b4a900a270f23c0de7efc221cc6c7572e0a
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-09-27 00:49:11 -0700

    Almost Done

--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -3,31 +3,29 @@
 #include <fstream>
 #include <iomanip>
 #include <ctype.h>
-#include "Lex.hpp"
+#include "Lex1.h"
 
 using namespace std;
 
 int main() {
-    string statement;
-    ifstream file;
-    file.open("test_case.txt");
-    
-    //Make sure file is opened
-    if (!file.is_open())
-    {
-        cerr << "Failed to open the file";
-        exit(1);
-    }
-    
-    Lex check;
-    string token;
-    while (!file.eof())
-    {
-        //file >> statement;
-        cout << check.lexer(file) << " ";
-    }
+	ifstream file;
+	file.open("test_case.txt");
 
-    file.close();
+	//Make sure file is opened
+	if (!file.is_open())
+	{
+		cerr << "Failed to open the file";
+		exit(1);
+	}
+
+	Lex check;
+	while (!file.eof())
+	{
+		check.lexer(file);
+		check.print();
+	}
+
+	cout << endl;
+	system("Pause");
 	return 0;
 }
-
