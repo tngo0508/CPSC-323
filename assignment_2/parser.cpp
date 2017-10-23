3c6f9fe911804b55fc4b010dcd51a919ec0d1599
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-10-23 00:51:37 -0700

    Add files via upload

---
+++ assignment_2/parser.cpp
@@ -0,0 +1,137 @@
+#include "parser.h"
+
+
+
+Lex parser::getToken() 
+{
+	
+}
+
+void parser::accept(string s)
+{
+}
+
+void parser::expect(string s)
+{
+}
+
+parser::parser()
+{
+}
+
+void parser::RAT17F()
+{
+}
+
+void parser::optFunctionDef()
+{
+}
+
+void parser::functionDef()
+{
+}
+
+void parser::function()
+{
+}
+
+void parser::parameterList()
+{
+}
+
+void parser::parameter()
+{
+}
+
+void parser::qualifier()
+{
+}
+
+void parser::body()
+{
+}
+
+void parser::optDeclartationList()
+{
+}
+
+void parser::declarationList()
+{
+}
+
+void parser::Declaration()
+{
+}
+
+void parser::iDs()
+{
+}
+
+void parser::stmtList()
+{
+}
+
+void parser::stmt()
+{
+}
+
+void parser::compound()
+{
+}
+
+void parser::assign()
+{
+}
+
+void parser::ifKeyword()
+{
+}
+
+void parser::returnKeyword()
+{
+}
+
+void parser::writeKeyword()
+{
+}
+
+void parser::readKeyword()
+{
+}
+
+void parser::whileKeyword()
+{
+}
+
+void parser::condition()
+{
+}
+
+void parser::relop()
+{
+}
+
+void parser::expression()
+{
+}
+
+void parser::term()
+{
+}
+
+void parser::factor()
+{
+}
+
+void parser::primary()
+{
+}
+
+void parser::empty()
+{
+}
+
+
+parser::~parser()
+{
+}
---
+++ assignment_2/parser.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "Lex.h"
+
+class parser
+{
+public:
+
+	Lex currentToken;
+	Lex getToken();
+	void accept(string s);
+	void expect(string s);
+	parser();
+	void RAT17F();
+	void optFunctionDef();
+	void functionDef();
+	void function();
+	void parameterList();
+	void parameter();
+	void qualifier();
+	void body();
+	void optDeclartationList();
+	void declarationList();
+	void Declaration();
+	void iDs();
+	void stmtList();
+	void stmt();
+	void compound();
+	void assign();
+	void ifKeyword();
+	void returnKeyword();
+	void writeKeyword();
+	void readKeyword();
+	void whileKeyword();
+	void condition();
+	void relop();
+	void expression();
+	void term();
+	void factor();
+	void primary();
+	void empty();
+	~parser();
+};
+
